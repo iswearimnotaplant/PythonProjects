fdbf3ae9347f4e1bce4edb572118ca6b1583ff43
iswearimnotaplant <Ryan_Lira@baylor.edu>
2020-03-05 09:21:41 -0600

    added both methods

--- factorialRecursion.py
+++ factorialRecursion.py
@@ -11,3 +11,13 @@ result = fact(x)
 
 print(result)
 '''
+
+#loop method for factorial
+'''
+num = int(input("enter a number: "))
+fac = 1
+for i in range(1, num + 1):
+    fac = fac * i
+
+print("factorial of ", num, " is ", fac)
+'''
